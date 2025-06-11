dedd25f8d47531a7659b00b8ce02e2fbf4eda21e
kaiperera <kaiperera25@gmail.com>
2025-06-11 15:34:52 +0100

    save

--- scripts/als_snp_overlap_binding_regions.R
+++ scripts/als_snp_overlap_binding_regions.R
@@ -1,5 +1,5 @@
 # read in als_gwas1 and bed_data - may need to amend paths due to organisation
-
+# make sure tidyverse, genomic ranges and jsonlite loaded
 
 # convert to grange  ------------------------------------------------------
 
@@ -11,9 +11,31 @@ granges_bed <- GRanges(
   strand = ifelse(bed_data$V6 %in% c("+", "-"), bed_data$V6, "*")
 )
 
+als_gwas1_df <- as.data.frame(als_gwas1)
 
-granges_alsgwas1 <- GRanges(
-  seqnames = als_gwas1$id, 
-  ranges = IRanges ( 
-    start = als_gwas1$)
-)
+
+als_gwas1_separate_id <- als_gwas1_df |> 
+  dplyr::relocate(id) |> #relocates column to start if no position given
+  separate(id,
+           remove = FALSE, # doesnt remove OG column
+           convert = TRUE,   #converts it to numerical rather than character vector
+           sep = '_',
+           into = c('chr','start')) 
+
+als_gwas_gr <- als_gwas1_separate_id |> 
+  makeGRangesFromDataFrame(
+    keep.extra.columns = TRUE,
+    seqnames.field = "chr",
+    start.field = "start",
+    end.field = "start"
+  )
+
+#use og als snps to start - will need rsid and strand info - may need als_snps_gr
+
+als_snps_to_start_df <- as.data.frame(als_snps_to_start)
+
+
+
+# get strand info and rsid  -----------------------------------------------
+
+
