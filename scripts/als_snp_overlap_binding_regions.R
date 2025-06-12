1c340143a6b536c7429b5347a7a4cf50065df0c8
kaiperera <kaiperera25@gmail.com>
2025-06-12 16:31:07 +0100

    need to finish fixing rsids

--- scripts/als_snp_overlap_binding_regions.R
+++ scripts/als_snp_overlap_binding_regions.R
@@ -28,7 +28,7 @@ als_gwas_gr <- als_gwas1_separate_id |>
     seqnames.field = "chr",
     start.field = "start",
     end.field = "start",
-    strand = 
+    strand = "annot.strand"
   )
 
 #use og als snps to start - will need rsid and strand info - may need als_snps_gr= have to left join something somewhere 
@@ -42,16 +42,121 @@ als_snps_to_start_df <- as.data.frame(als_snps_to_start)
 combined_gr <- c(als_snps_gr, als_gwas_gr) #idk if this was useful
 
 
-rsids <- als_snps_gr$hm_rsid 
-als_snp_strand_info <- snpsById(SNPlocs.Hsapiens.dbSNP155.GRCh38, rsids, ifnotfound = "drop")
-strand(als_snps_gr) <- strand(als_snp_strand_info)[match(rsids, names(als_snp_strand_info))]
+
+view(snp_annotated_strand) # shows strand infor - code on generate_als_snp
+
+view(granges_bed) #also has strand info
+
+view(als_gwas_gr)
 
 
-missing_snps <- rsids[!rsids %in% names(als_snp_strand_info)]
-head(missing_snps)
+library(biomaRt)
 
-strand(als_snps_gr) <- "*"  # Initialize all as unstranded
-found <- rsids %in% names(als_snp_strand_info)
-strand(als_snps_gr)[found] <- strand(als_snp_strand_info)[match(rsids[found], names(als_snp_strand_info)
+# Connect to Ensembl
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 
-                                                                
+# Get genes overlapping your GRanges
+genes <- getBM(
+  attributes = c("chromosome_name", "start_position", "end_position", "strand"),
+  filters = c("chromosomal_region"),
+  values = paste0(seqnames(als_gwas_gr), ":", start(als_gwas_gr), "-", end(als_gwas_gr)),
+  mart = ensembl
+)
+
+# Convert numeric strands (1/-1) to character
+genes$strand <- ifelse(genes$strand == 1, "+",
+                       ifelse(genes$strand == -1, "-", "*"))
+
+# Convert to GRanges and assign strands
+als_gwas1_strand <- makeGRangesFromDataFrame(
+  genes,
+  seqnames.field = "chromosome_name",  
+  start.field = "start_position",     
+  end.field = "end_position",        
+  strand.field = "strand",            
+  keep.extra.columns = TRUE           
+)
+hits <- findOverlaps(als_gwas_gr, genes_gr)
+strand(als_gwas_gr)[queryHits(hits)] <- strand(genes_gr)[subjectHits(hits)]
+
+
+# rsids -------------------------------------------------------------------
+#currently working on als gwas1 strand info - trying to get rsids via ensembl- if doesnt work try to merge w als_snps_gr -
+
+
+
+# Connect to the dedicated SNP database
+snp_mart <- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
+
+
+
+
+regions <- paste0(
+  seqnames(als_gwas1_strand), ":", 
+  start(als_gwas1_strand), "-", 
+  end(als_gwas1_strand)
+)
+
+# Fetch rsIDs for all regions
+snp_data <- getBM(
+  attributes = c("refsnp_id", "chr_name", "chrom_start", "chrom_end"),
+  filters = "chromosomal_region", 
+  values = regions,
+  mart = snp_mart
+)
+
+
+# For main Ensembl genes (not optimal for SNPs)
+listAttributes(ensembl)[grep("snp", listAttributes(ensembl)$name, ignore.case = TRUE),]
+
+# Better: Use the dedicated SNP mart
+snp_mart <- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
+listAttributes(snp_mart)[1:20,]  # View first 20 attributes
+
+
+
+
+
+
+
+
+#left join als strand grange and als snp grange - need to finish fixing
+
+
+#  find overlaps
+overlaps <- findOverlaps(als_gwas1_strand, snp_annotated_strand)
+
+# Create a merged GRanges with metadata from both
+result <- als_gwas1_strand[queryHits(overlaps)]
+mcols(result) <- cbind(mcols(result), mcols(snp_annotated_strand[subjectHits(overlaps)]))
+
+# For ranges in gwas_strand that don't overlap with anp_annot, we need to add them back
+non_overlapping <- als_gwas1_strand[!seq_along(als_gwas1_strand) %in% queryHits(overlaps)]
+mcols(non_overlapping) <- cbind(mcols(non_overlapping), 
+                                DataFrame(matrix(NA, nrow=length(non_overlapping), 
+                                                 ncol=ncol(mcols(snp_annotated_strand)), 
+                                               )))
+                                
+# Combine the results
+final_result <- c(result, non_overlapping)
+ final_result <- final_result[order(as.numeric(c(queryHits(overlaps), 
+                                                 which(!seq_along(als_gwas1_strand) %in% queryHits(overlaps)))))]
+#final result doesnt have rsids 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
