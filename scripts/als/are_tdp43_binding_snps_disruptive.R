cf935f7dfb41873342eb2bc8751f527a1c385bbe
kaiperera <kaiperera25@gmail.com>
2025-06-25 12:03:35 +0100

    save

--- scripts/als/are_tdp43_binding_snps_disruptive.R
+++ scripts/als/are_tdp43_binding_snps_disruptive.R
@@ -8,9 +8,12 @@ chi2 <- final_result_tbl %>%
   select(hm_rsid,snp_in_tdp) %>% 
   left_join(unique_score_rsid, by = "hm_rsid", relationship = "many-to-many") %>% 
   filter(complete.cases(.)) %>%
-  mutate(min_diff_binned = cut(min_diff, 
-                               breaks = quantile(min_diff, probs = seq(0, 1, 0.2)), #bins into quintiles
-                               include.lowest = TRUE))
+  mutate(min_diff_binned = if_else(min_diff <=  -0.1398594 , "Low (<=CE_SNP)", "High (> CE_SNP)"))
+
+
+#bin into 2 - those w same amount of disruption as CE SNP and those that don't
+
+
 
 
 
@@ -25,50 +28,71 @@ contingency_table <- table(chi2$snp_in_tdp, chi2$min_diff_binned)
     
     print(result)
 
-#p-value <0.05 so significant association between snp in binding region and min diff 
-
+#p-value >0.05 so no significant association between snp in binding region and min diff - not necessarily more disruptive if in binding region
+#X-squared = 1.1223, df = 1, p-value = 0.2894
 
 # fisher's test -----------------------------------------------------------
 
-fisher <- final_result_tbl %>% 
-  mutate(snp_in_tdp = hm_rsid %in% final_overlap_tbl$hm_rsid) %>% 
-  select(hm_rsid, snp_in_tdp) %>% 
-  left_join(unique_score_rsid, by = "hm_rsid", relationship = "many-to-many") %>%
-  filter(complete.cases(.))
 
-contingency_table <- table(fisher$snp_in_tdp, fisher$min_diff)
-contingency_table_clean <- na.omit(contingency_table)
-fisher.test(contingency_table, simulate.p.value=TRUE, B=1e5) #data was too large so had to add this smth about monte carlo simulation
-
-#fisher test on extreme intervals
-fisher.test(contingency_table[, c("[-0.548,-0.158]", "(-0.0333,0.000162]")])
-#not statistically significant as p >0.05 - no evidence that these intervals are more or less disruptive compared to others
-
-# both the fisher tests (simulate and below) give different value but both have significant p value
-
-fisher.test(contingency_table, workspace = 2e9)
-
-
+fisher_result <- fisher.test(contingency_table) 
+    
 
 # visualization -----------------------------------------------------------
 
-
-contingency_table_df <- as.data.frame(contingency_table)
-
-
-
+#Corrplot
 chi2_plot <- chisq.test(contingency_table)
 
 corrplot(chi2_plot$residuals, is.cor = FALSE, 
-         title = "Chi-Square Residuals")
+         title = "Chi-Square Residuals",
+         tl.srt = 45,
+         mar = c(0, 0, 2, 0))  #prefer this plot 
 
 #residuals heatmap- residual quantifies how much  observed cell counts deviate from the counts expected under independence 
 corrplot(chi2_plot$residuals, 
          is.cor = FALSE,          # Not a correlation matrix
          method = "color",        # Color squares by value
-         tl.col = "black",        # Label color
+         tl.col = "black",        # Label color     
+         tl.srt = 45,
          title = "Chi-Square Residuals Plot",
          mar = c(0, 0, 2, 0)) 
 
 
+#both plots show same thing but one has circles and the other is squares 
+#think red means higher disruption (lower min diff) so those in binding regions seem slightly more disruptive but not statistically significant 
 
+
+
+#ggplot stacked bar chart - plot fishers exact into plot and see how many snps r in and out of binding region
+
+chi2_counts <- chi2 |> 
+  count(snp_in_tdp, min_diff_binned)
+
+p_value <- round(fisher_result$p.value, 4) # extracts p value and rounds to 4dp
+
+chi_p_value <- round(result$p.value, 4)
+
+ggplot(chi2_counts, aes(x = snp_in_tdp, y = n, fill = min_diff_binned)) +
+  geom_col(position = "dodge") +
+  labs(x = "SNP in TDP-43 Binding Region",
+       y = "Number of SNPs",
+       fill = "CE_SNP-Relative Difference") +
+  annotate(
+    "text",             # Type of annotation (text)
+    x = Inf, y = Inf,   # Position: top-right corner of the plot
+    label = paste("Fisher's p-value =", p_value),
+    hjust = 1.1,        # Horizontal adjustment (pushes text left from Inf)
+    vjust = 1.1,        # Vertical adjustment (pushes text down from Inf)
+    size = 3,           
+    color = "blue"      
+  ) +
+  annotate(
+    "text",
+    x = Inf, y = Inf,
+    label = paste("Chi-Squared p-value =", chi_p_value),
+    hjust = 1.1,
+    vjust = 3.0,
+    size = 3,
+    color = "violetred1"
+  ) +
+  theme_bw() 
+ 
