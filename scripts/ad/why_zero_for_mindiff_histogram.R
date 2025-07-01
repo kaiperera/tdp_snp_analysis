9fd5e664c277fc4fceda587a4f7b0b42ffb2a9e3
kaiperera <kaiperera25@gmail.com>
2025-07-01 16:16:07 +0100

    save

--- scripts/ad/why_zero_for_mindiff_histogram.R
+++ scripts/ad/why_zero_for_mindiff_histogram.R
@@ -14,7 +14,11 @@ data_for_histogram |>
 zero_min_diff <- data_for_histogram |> 
   filter(min_diff == 0) |> 
   count(snps, sort = TRUE)   # Adds a column `n` with frequency - each occurs 75 times due to 75 nt flank per snp
-   #114 snps with 0 as min_diff
+   
+
+
+
+#114 snps with 0 as min_diff
 
 
 #data_for_histogram = 24,300 entries - 114 have 0 as min_diff - why
@@ -44,6 +48,7 @@ ad_gwas_removed_context |> janitor::clean_names() |>
   distinct(mapped_gene) |> 
   pull()
   
+
 #most are intron variants but not all 
   
 # 108 different genes 
@@ -51,5 +56,50 @@ ad_gwas_removed_context |> janitor::clean_names() |>
 
 # are any of the snps in regulatory regions -------------------------------
 
-view(zero_min_diff)
+ensembl <- useEnsembl(biomart = "regulation", dataset = "hsapiens_regulatory_feature")
+
+# Get all regulatory features for a gene (e.g., BRCA1)
+reg_features <- getBM(
+  attributes = c("chromosome_name", "chromosome_start", "chromosome_end", "feature_type_name"),
+  filters = "regulatory_feature_type_name",
+  values = "Promoter",  # Or "Enhancer", "CTCF Binding Site"
+  mart = ensembl
+)
+
+reg_features$ucsc_chr <- ifelse(
+  reg_features$chromosome_name == "MT",  # Check mitochondrial case
+  "chrM",
+  paste0("chr", reg_features$chromosome_name)
+)
+
+
+check <- ad_gwas_removed_context |> janitor::clean_names() |> 
+  filter(snps %in% zero_min_diff$snps) |> 
+  distinct(snps, .keep_all = TRUE) |> 
+  rename(sequence = seqnames,
+         strand2 = strand)
+
+
+reg_gr <- reg_features |> 
+  makeGRangesFromDataFrame(
+    seqnames.field = "chromosome_name",  
+    start.field = "chromosome_start",
+    end.field = "chromosome_end",
+    keep.extra.columns = TRUE  
+  )
+  
+check_gr <- check |> 
+  makeGRangesFromDataFrame(
+    seqnames.field = "chr_id",
+    start.field = "pos",
+    end.field = "pos",
+    keep.extra.columns = TRUE
+  )
+
+
+
+
+reg_overlaps<- subsetByOverlaps(check_gr, reg_gr)
+
+#only rs145049847 in a regulatory region - not reason why so many are 0
 
