b5ea408a35178a63cd3506a91ac5fe755a63621e
kaiperera <kaiperera25@gmail.com>
2025-07-14 15:00:07 +0100

    save

--- salmon_scripts/salmon_rna/finding_rbp_binding_motifs.R
+++ salmon_scripts/salmon_rna/finding_rbp_binding_motifs.R
@@ -99,6 +99,88 @@ names(downstream_DSS) <- skipped_exon_gr_down$transcript_name
 writeXStringSet(downstream_DSS, filepath = "downstream_skipped.fasta")
 
 
+
+# merging upstream and downstream granges into main one -------------------
+upstream_df <- as.data.frame(upstream_gr)
+downstream_df <- as.data.frame(downstream_gr)
+
+
+upstream_join_df <- data.frame(
+  transcript_id = upstream_df$transcript_id,  # Join key
+  flank_sequence_up = upstream_df$flank_sequence  # Rename for clarity
+)
+
+downstream_join_df <- data.frame(
+  transcript_id = downstream_df$transcript_id,
+  flank_sequence_down = downstream_df$flank_sequence
+)
+
+merged_df <- skipped_exon_resized_df |> 
+  left_join(upstream_join_df, by = "transcript_id") |> 
+  left_join(downstream_join_df, by = "transcript_id") |> 
+  rename(chr = seqnames) |> 
+  rename(flank_sequence_exon = flank_sequence)
+
+
+
+all_sequences <- c(
+  merged_df$flank_sequence_up,
+  merged_df$flank_sequence_exon,
+  merged_df$flank_sequence_down
+)
+
+
+sequence_names <- paste(
+  rep(merged_df$transcript_name, 3),                # Repeat each ID 3x
+  rep(c("upstream", "exon", "downstream"), each = nrow(merged_df)),  # Add region labels
+  sep = "_"
+)
+
+merged_DSS <- DNAStringSet(all_sequences)
+names(merged_DSS) <- sequence_names
+
+writeXStringSet(merged_DSS, filepath = "merged.fasta")
+
+
+
+# finding motifs from 1 table ---------------------------------------------
+
+merged_fasta <- read_fasta("C:/Users/Kai/Desktop/tdp_snp_analysis/merged.fasta")
+
+find_motifs(merged_fasta, tdp_43_motif)   
+find_motifs(merged_fasta, tdp_reverse)
+
+
+countPattern(merged_fasta, tdp_43_motif)
+
+
+
+
+vcountPattern(tdp_motif_DSS, merged_fasta_DSS)
+
+merged_fasta_DSS <- DNAStringSet(all_sequences)
+names(merged_fasta_DSS) <- sequence_names
+
+
+
+# table for count in up, down and exon ------------------------------------
+motif_count <- vcountPattern(tdp_motif_DSS, merged_fasta_DSS)
+transcript_ids <- str_remove(merged_fasta$name, "_upstream|_exon|_downstream") |> 
+  unique()
+
+ tibble(
+  transcript_id = rep(transcript_ids, each = 3),
+  region = rep(c("upstream", "exon", "downstream"), times = 37),
+  count = motif_count
+) |> 
+  pivot_wider(
+    names_from = region,
+    values_from = count
+  ) |> view()
+
+
+
+
 # finding motifs Exons  - tidyseq - this one works better  -----------------------------------------------
 
 #read in fasta 
