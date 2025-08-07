cf44afbedc46596c7078ffe991bc52b8bb363af5
kaiperera <kaiperera25@gmail.com>
2025-08-07 16:09:39 +0100

    save

--- als_ad_dna_analysis/als_scripts/als_in_binding_more_disruptive_overlap.R
+++ als_ad_dna_analysis/als_scripts/als_in_binding_more_disruptive_overlap.R
@@ -84,85 +84,50 @@ snp_info_gr <- snp_info |>
   )
 
 
-library(VariantAnnotation)
-#find overlapping snps using txdb
 
-gene_overlaps <- locateVariants(
-  snp_info_gr, 
-  TxDb.Hsapiens.UCSC.hg38.knownGene, 
-  AllVariants()
-) |> unique()
-
-
-gene_overlaps <- as.data.frame(gene_overlaps)
-
-
-
-
-
-
-
-gene_ids <- na.omit(unique(gene_overlaps$GENEID))
 library(org.Hs.eg.db)
 
-# map entrez
-gene_symbols <- mapIds(
+
+
+snp_info_df <- as.data.frame(snp_info) 
+
+
+
+
+
+# finding overlaps and getting gene names and descriptions -----------------------------------------------------
+
+names(snp_info_gr) <- snp_info$RefSNP_id
+
+txdb_genes <- genes(TxDb.Hsapiens.UCSC.hg38.knownGene)
+
+overlaps <- findOverlaps(snp_info_gr,txdb_genes)
+
+gene_overlaps <- data.frame(
+  rsid = names(snp_info_gr) [queryHits(overlaps)],
+  geneid = mcols(txdb_genes)$gene_id[subjectHits(overlaps)])
+
+gene_overlaps$geneid <- as.character(gene_overlaps$geneid)
+
+gene_symbols <- mapIds(org.Hs.eg.db,
+                       keys = gene_overlaps$geneid,
+                       keytype = "ENTREZID",
+                       column = "SYMBOL")
+
+gene_overlaps$symbol <- gene_symbols
+
+
+
+gene_annotations <- AnnotationDbi::select(
   org.Hs.eg.db,
-  keys = gene_ids,
-  keytype = "ENTREZID",
-  column = "SYMBOL"
-)
-
-# convert data frame
-gene_mapping <- data.frame(
-  ENTREZID = names(gene_symbols),
-  SYMBOL = gene_symbols)
-
-
-gene_overlaps <- gene_overlaps |>  
-  left_join(gene_mapping, by = c("GENEID" = "ENTREZID")) |> 
-  janitor::clean_names()
-
-gene_overlaps |>  distinct(symbol) |> print()
-
-
-print(gene_overlaps$symbol)
+  keys = unique(gene_overlaps$geneid),
+  columns = c("SYMBOL", "GENENAME", "ENTREZID"),
+  keytype = "ENTREZID"
+) %>%
+  mutate(ENTREZID = as.character(ENTREZID)) 
+gene_annotations <- gene_annotations |> rename("geneid" = "ENTREZID")
 
 
 
-#  gene function ------------------------------------------------------
-
-library(clusterProfiler)
-
-
-ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-
-# Get gene descriptions
-gene_descriptions <- getBM(
-  attributes = c("entrezgene_id", "external_gene_name", "description"),
-  filters = "entrezgene_id",
-  values = na.omit(unique(gene_overlaps$geneid)),
-  mart = ensembl
-)
-
-gene_descriptions <- gene_descriptions |> 
-  as.data.frame() |> 
-  rename(symbol = external_gene_name)
-
-head(gene_descriptions)
-
-
-gene_overlaps |> 
-  left_join(gene_descriptions, by = "symbol") |>
-  select(description) |> print()
-
-
-#get detailed annotations of each gene
-gene_function <- AnnotationDbi::select(org.Hs.eg.db, 
-                      keys=gene_ids, 
-                      columns=c("SYMBOL", "GENENAME", "UNIPROT", "PATH"), 
-                      keytype="ENTREZID") |> unique()
-
-
-gene_overlaps |> 
-  left_join(gene_function, by)
+tdp_kd_genes <- gene_overlaps |> 
+  left_join(gene_annotations, by = "geneid")
---
+++ als_ad_dna_analysis/als_scripts/tdp_knockdown_genes.R
@@ -0,0 +1,27 @@
+library(data.table)
+
+tdp_kd_genes <- gene_overlaps |> 
+  left_join(gene_annotations, by = "geneid") # from als_in_binding_disruptive_overlap
+
+diff_expr_sh <- read_csv("C:/Users/Kai/Downloads/diff_expr_sh.csv")
+diff_expr_sk <- read.csv("C:/Users/Kai/Downloads/diff_expr_sk.csv")
+
+
+diff_expr_sh <- as.data.frame(diff_expr_sh)
+diff_expr_sk <- as.data.frame(diff_expr_sk)
+
+
+
+# sh ----------------------------------------------------------------------
+
+sh_plot_data <- tdp_kd_genes |> 
+  left_join(diff_expr_sh, by = "symbol")
+
+  
+#x = source, y = log2fold change - same for next plot 
+
+ggplot(sh_plot_data, aes(x = source, y = log2FoldChange)) +
+  geom_point() +
+  labs(x = "source",
+       y = "log2FoldChange") +
+  
