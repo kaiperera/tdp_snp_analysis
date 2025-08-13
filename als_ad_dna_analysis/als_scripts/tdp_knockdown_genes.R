90655d6d0048181b6d091dee615b1305834758fa
kaiperera <kaiperera25@gmail.com>
2025-08-13 12:18:21 +0100

    save

--- als_ad_dna_analysis/als_scripts/tdp_knockdown_genes.R
+++ als_ad_dna_analysis/als_scripts/tdp_knockdown_genes.R
@@ -16,48 +16,90 @@ diff_expr_sk <- as.data.frame(diff_expr_sk)
 
 sh_plot_data <- tdp_kd_genes |> 
   left_join(diff_expr_sh, by = "symbol") |>  
-  mutate(sig = ifelse(padj < 0.05, "Significant", "NS"))
+  mutate(sig = ifelse(padj < 0.05, "Significant", "NS")) |>
+  filter(sig == "Significant") |> 
+  mutate(source = as.factor(source))
 
+significant_genes <- sh_plot_data %>%
+  group_by(symbol) %>%  
+  summarize(
+    n_significant = sum(sig == "Significant"),  
+    .groups = "drop"
+  ) %>%
+  filter(n_significant >= 2)
+
+sh_plot_data <- sh_plot_data |> 
+  filter(symbol %in% significant_genes$symbol)
+
+
+library(randomcoloR)
+colours <- distinctColorPalette(36)
   
 #x = source, y = log2fold change - same for next plot 
 
-ggplot(sh_plot_data, aes(x = source, y = log2FoldChange, colour = sig)) +
+ggplot(sh_plot_data, aes(x = source, y = log2FoldChange, colour = symbol, group = symbol)) +
   geom_point() +
+  geom_line() +
+  facet_wrap(~symbol, ncol = 6) +
   labs(title = "SH Differential Expression",
        x = "source",
        y = "log2FoldChange",
-       colour = "Significance (P-adj value)") +
-  scale_colour_manual(
-    name = "Significance (P-adj value)",
-    labels = c("NS" = "Not Significant"),
-    values = c("orange", "blue")
+       colour = "Gene")  +
+  scale_colour_manual(values = colours) + 
+  theme_bw() + 
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 8),  
+    strip.text = element_text(size = 8, face = "bold"),            
+    panel.spacing = unit(0.2, "cm"),                              
+    plot.title = element_text(size = 12, hjust = 0.5)             
   ) +
-  theme_bw() 
+  theme(legend.position = "none")
   
 
+ggplot(sh_plot_data, aes(x = source, y = log2FoldChange, colour = symbol, group = symbol)) +
+  geom_point() +
+  geom_line() +
+  labs(title = "SH Differential Expression",
+       x = "source",
+       y = "log2FoldChange",
+       colour = "Gene")  +
+  scale_colour_manual(values = colours) + 
+  theme_bw() +
+  guides(colour = guide_legend(ncol = 3))
 
+#36 genes
 
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 # sk ----------------------------------------------------------------------
 
 sk_plot_data <- tdp_kd_genes |> 
   left_join(diff_expr_sk, by = "symbol") |>  
-  mutate(sig = ifelse(padj < 0.05, "Significant", "NS"))
+  mutate(sig = ifelse(padj < 0.05, "Significant", "NS")) |> 
+  filter(sig == "Significant") |> 
+  mutate(source = as.factor(source))
 
 
-ggplot(sk_plot_data, aes(x = source, y = log2FoldChange, colour = sig)) +
+
+
+ggplot(sk_plot_data, aes(x = source, y = log2FoldChange, colour = symbol, group = symbol)) +
   geom_point() +
-  labs( title = " SK Differential Expression",
-        x = "source",
+  geom_line() +
+  labs(title = "SK Differential Expression",
+       x = "source",
        y = "log2FoldChange",
-       colour = "Significance (P-adj value)") +
-  scale_colour_manual(
-    name = "Significance (P-adj value)",
-    labels = c("NS" = "Not Significant"),
-    values = c("orange", "blue")
-  ) +
-  theme_bw() 
+       colour = "Gene")  +
+  theme_bw()  
 
 
-
+#24 genes
