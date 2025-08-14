3c6d16b0cd7250b45aa9a9050f5983365bf7bdd1
kaiperera <kaiperera25@gmail.com>
2025-08-14 15:43:34 +0100

    save

--- als_ad_dna_analysis/als_scripts/tdp_knockdown_genes.R
+++ als_ad_dna_analysis/als_scripts/tdp_knockdown_genes.R
@@ -42,7 +42,7 @@ sh_plot_data <- tdp_kd_genes |>
      colour = "black"
    ) +
    geom_line(aes(colour = symbol), linewidth = 0.7) +
-   labs(title = "SH Differential Expression",
+   labs(title = "SH-SY5Y Differential Expression",
         x = "Source",
         y = "log2FoldChange",
         colour = "Gene",
@@ -118,7 +118,7 @@ sk_sig <- tdp_kd_genes |>
      colour = "black"
    ) +
    geom_line(aes(colour = symbol), linewidth = 0.7) +
-   labs(title = "SK Differential Expression",
+   labs(title = "SK-N-BE(2) Differential Expression",
         x = "Source",
         y = "log2FoldChange",
         colour = "Gene",
@@ -284,3 +284,76 @@ export(FANCD2, "FANCD2.bed") # chr3:10074462-10074462 rs7625685 , chr3:10087869-
 UNC13A <- four_genes_bp |> filter(hm_rsid == "rs12973192") 
 
 export(UNC13A, "UNC13A.bed")
+
+
+# rs6808853 bp ------------------------------------------------------------
+
+rs6808853 <- FANCD2 |> filter(hm_rsid == "rs6808853")
+
+rs6808853$variant_sequence = gsub("T", "U", rs6808853$variant_sequence)  #makes sure its RNA sequence
+rs6808853$variant_sequence = gsub("t", "u", rs6808853$variant_sequence)
+rs6808853$sequence = gsub("T", "U", rs6808853$sequence)
+rs6808853$sequence = gsub("t", "u", rs6808853$sequence)
+
+# FUNCTION CREATION
+paired_plot <- function(rs6808853, plot_difference) {      #    plot_difference =  function parameter (like a switch) that determines whether the plot shows: FALSE = raw weights for both sequences, TRUE = difference between weights
+  weights1 <- unlist(x$weights)
+  weights2 <- unlist(x$variant_weights)
+  
+  seq1 <- strsplit(toupper(rs6808853$sequence), "")[[1]]      # prepares code for comparison - toupper = all upper case, strsplit = splits character string into individual characters, [1] = select first element from each list returned by strsplit 
+  seq2 <- strsplit(toupper(rs6808853$variant_sequence), "")[[1]]  # output = character vectors 
+  
+  if(plot_difference) {    #for plot_difference = TRUE
+    weights2 <- weights2 - weights1  #calculates difference (variant - reference)
+    tbl <- data.frame(
+      pos = seq_along(seq2),
+      weight = weights2,        #difference values
+      group = factor(rep("difference", length(seq2))) # all rows labelled difference 
+    )
+  } else {      #for plot_difference = FALSE 
+    tbl <- data.frame(
+      pos = c(seq_along(seq1), seq_along(seq2)), # combines positions for both sequences 
+      weight = c(weights1, weights2),     #stacks reference and variants weights 
+      group = factor(c(rep("reference", length(seq1)), rep("variant", length(seq2))), levels=c("reference","variant"))
+    )   #explicit factor levels 
+  }
+  
+  
+  xlabels <- mapply(function(a, b) paste(a, ifelse(a==b, "", b), sep="\n"), seq1, seq2) # visual comparison of sequences via stacking them vertically and highlighting differences 
+  
+  p <- ggplot(tbl, aes(pos, weight))
+  if(plot_difference) p <- p + geom_hline(yintercept=0, color="dodgerblue") # only if plot_difference = TRUE - adds horizontal blue line at y intercept to show no difference - helps visualise positive and negative difference 
+  p <- p +
+    geom_line(aes(color=group), size=0.8) +
+    scale_x_continuous(breaks=seq(1, max(tbl$pos)), labels=xlabels) +   #x-axis ticks at each position
+    scale_color_manual(values=c("black", "red")) +
+    clean_theme() +
+    theme(
+      legend.title = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text.x = element_text(size=11)
+    ) + labs(y="DeepCLIP score")
+  return(p)
+}
+
+
+
+
+for (i in 1:dim(rs6808853)) {  
+  x = rs6808853[i,]   #extracts i-th row - assumes data in df where each row contains sequence data 
+  width = 10.5   #sets pdf dimensions
+  height = 3.65
+  if (length(rs6808853$weights[[1]]) <= 30) {width = 7.75}
+  
+  rsID <- x$hm_rsid
+  
+  p = paired_plot(rs6808853, plot_difference = FALSE)  #plot generation
+  pdf(paste0(rsID,".pdf"), width = width, height = height)  #saves plot as pdf
+  print(p)
+  dev.off()
+  
+  p_diff = paired_plot(rs6808853, plot_difference = TRUE)
+  pdf(paste0(rsID,".difference.pdf"), width = width, height = height)
+  print(p_diff)
+  dev.off()
+}
